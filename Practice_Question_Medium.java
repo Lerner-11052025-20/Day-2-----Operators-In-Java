dfb43877913bf4fe0e37328eb445b559cacd46da
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-19 00:29:24 +0530

    Add files via upload
    I solved Easy , medium and some tricky or hard Questions.

---
+++ Practice_Question_Medium.java
@@ -0,0 +1,22 @@
+public class Practice_Question_Medium 
+{
+    public static void main(String[] args) {
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+        
+        System.out.println(2f/4d);
+        System.out.println(2f/4);
+        System.out.println(2/4);
+        System.out.println(2/4.0);
+        System.out.println(2D/4L);
+        System.out.println(2d/4l);
+        System.out.println(2/4);
+        System.out.println(2/4l);
+        System.out.println(2d/4L);
+        System.out.println(2f/4L);
+    }
+}
