f7e15183b381c1ac3e13010eb406eaff90501a1d
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-19 20:56:08 +0530

    Create AreaOfReactangle.java
    This is the code in which i going to calculate area of the rectangle with the help of variables and datatypes. this is the day , i learn the more about the  Java Programming Language.

---
+++ AreaOfReactangle.java
@@ -0,0 +1,43 @@
+public class AreaOfReactangle
+{
+    public static void main(String args[])
+    {   
+        //finding area of rectangle
+        int length = 5,breadth = 7;
+        int area = length * breadth;
+        System.out.println("The area of rectangle is = " + area);
+
+        System.out.print("\n\n");
+        
+      //sum of the two numbers
+        int a = 5;
+        System.out.println(a);
+        a = 10;     //modified the value of an a variable
+        System.out.println(a);
+        int b = 10;
+        int sum = a+b;
+        System.out.print("The sum of a and b is ");
+        System.out.println("a + b = " + sum);
+
+
+        //String
+        String name = "Deep sorathiya";
+        System.out.println("Hello " + name);
+        name = "Meet sojitra";
+        System.out.println("Hello " + name);
+
+        System.out.println("\n\n");
+        
+        //declare many variable of same type
+        System.out.println("____declare many variable of the same data type______");
+        int x = 3;      //int x=3,y=6,z=9 ;
+        int y = 6;      
+        int z = 9;
+        float $sum1 = x + y + z ;
+        System.out.println($sum1);
+
+        x = y = z = 500;
+        System.out.println(x + y + z);
+
+    }
+}
